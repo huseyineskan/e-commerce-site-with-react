63428588fb5508a5c10ae9e15c01613bb5ba08d3
Huseyin Eskan <huseyineskan01@gmail.com>
2025-01-04 23:08:54 +0100

    added react-rotuter-dom and created pages and link structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import "./App.css";
 import PageContainer from "./container/PageContainer";
 import Header from "./components/Header";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <PageContainer>
-      <Header />
-    </PageContainer>
+    <BrowserRouter>
+      <PageContainer>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/about-us" element={<AboutUs />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </PageContainer>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,10 @@ import { SlBasket } from "react-icons/sl";
 
 import { CiLight } from "react-icons/ci";
 import { CiCloudMoon } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 function Header() {
-  const [theme, setTheme] = React.useState("dark");
+  const [theme, setTheme] = React.useState("light");
 
   const changeTheme = () => {
     const body = document.querySelector("body");
@@ -15,36 +16,54 @@ function Header() {
 
   return (
     <div className="header">
-      <a href="/" className="logo">
-        <img src="./src/images/logo.png" alt="" srcset="" />
-        <div className="logo-text">
-          <p>E-Commerce</p>
-          <span>Super Prices</span>
+      <div className="header-top">
+        <a href="/" className="logo">
+          <img src="./src/images/logo.png" alt="Logo" />
+          <div className="logo-text">
+            <p>E-Commerce</p>
+            <span>Super Prices</span>
+          </div>
+        </a>
+        <div className="search flex-row">
+          <input type="text" placeholder="Search" />
+
+          {theme === "light" ? (
+            <CiCloudMoon
+              className="icon"
+              onClick={() => {
+                changeTheme();
+                setTheme("dark");
+              }}
+            />
+          ) : (
+            <CiLight
+              className="icon"
+              onClick={() => {
+                changeTheme();
+                setTheme("light");
+              }}
+            />
+          )}
+
+          <SlBasket className="icon" />
         </div>
-      </a>
-      <div className="search flex-row">
-        <input type="text" placeholder="Search" />
-
-        {theme === "dark" ? (
-          <CiCloudMoon
-            className="icon"
-            onClick={() => {
-              changeTheme();
-              setTheme("light");
-            }}
-          />
-        ) : (
-          <CiLight
-            className="icon"
-            onClick={() => {
-              changeTheme();
-              setTheme("dark");
-            }}
-          />
-        )}
-
-        <SlBasket className="icon" />
       </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/about-us">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
